ed76fc89dde86461e531448c88f68ca3037a90bd
nborrelli-illy <aniello.borrelli@illy.com>
2025-02-24 15:28:28 +0100

    [W6Y][FEATURE][FE] Installation Packages

--- src/pages/theme/components/divider.ts
+++ src/pages/theme/components/divider.ts
@@ -2,14 +2,14 @@ export default {
   baseStyle: {
     divider: {
       w: '100%',
-      py: { base: '100px', lg: '144px' },
+      py: { base: '40px'},
       backgroundColor: '#FFEEBB',
       display: 'flex',
       justifyContent: 'center',
     },
     img: {
       borderRadius: '50%',
-      w: { base: '175px', lg: '200px' },
+      w: { base: '100px'},
     },
   },
   variants: {},
