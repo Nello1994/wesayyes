3d21c90a06362de51eb24dbb8570530f7d95e1b8
Nello1994 <borrelli.nello94@gmail.com>
2025-02-24 17:07:57 +0100

    Google Api + Form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import SaveTheDate from '../components/save-the-date/index'
 import Divider from '../components/divider'
+import ContactForm from '../components/contact-form/ContactForm'
 
 const Home = (props) => {
   return (
     <>
       <SaveTheDate></SaveTheDate>
       <Divider />
+      <ContactForm></ContactForm>
     </>
   )
 }
