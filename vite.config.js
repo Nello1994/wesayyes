fdc80f192413bbecc56d3a5a21ee8f29295b9c6d
Nello1994 <borrelli.nello94@gmail.com>
2025-04-04 13:42:11 +0200

    Fix Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/wesayyes/",  // <-- Aggiungi questa riga con il nome del tuo repository
   plugins: [react()],
 })
