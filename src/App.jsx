4de3d5d4242092da0e08c209b2c3613da57dfcab
Nello1994 <borrelli.nello94@gmail.com>
2025-04-04 15:19:16 +0200

    Fix Redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import './style/App.scss'
-import { VStack, useMultiStyleConfig, useTheme } from '@chakra-ui/react'
-import Home from './pages/home'
-import NotFound from './pages/NoPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import UikitPage from './uikit'
-import ColorPage from './uikit/pages/color'
-import TypographyPage from './uikit/pages/typography'
-import Global from './pages/theme/global'
+import "./style/App.scss"
+import { VStack, useMultiStyleConfig, useTheme } from "@chakra-ui/react"
+import Home from "./pages/home"
+import NotFound from "./pages/NoPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import UikitPage from "./uikit"
+import ColorPage from "./uikit/pages/color"
+import TypographyPage from "./uikit/pages/typography"
+import Global from "./pages/theme/global"
 
 function App() {
-  const styles = useMultiStyleConfig('Global')
+  const styles = useMultiStyleConfig("Global")
 
   return (
     <VStack {...styles.container}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="uikit" element={<UikitPage />} />
-          <Route path="uikit/color" element={<ColorPage />} />
-          <Route path="uikit/typography" element={<TypographyPage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='/wesayyes' element={<Home />} />
+          <Route path='uikit' element={<UikitPage />} />
+          <Route path='uikit/color' element={<ColorPage />} />
+          <Route path='uikit/typography' element={<TypographyPage />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </VStack>
